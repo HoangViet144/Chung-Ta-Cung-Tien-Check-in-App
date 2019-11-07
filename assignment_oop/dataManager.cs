513db188711bf223b9bd6afbaeff6987f5e83bcf
HoangViet <viet14042000gmail.com>
2019-11-07 21:11:26 +0700

    update Double Check Locking Singleton

--- assignment_oop/dataManager.cs
+++ assignment_oop/dataManager.cs
@@ -6,9 +6,41 @@ using System.Threading.Tasks;
 
 namespace assignment_oop
 {
-    //singleton class
-    class dataManager
+    //Double Check Locking Singleton
+    public sealed class dataManager
     {
-        private static Dât
+        private static int counter = 0;
+        private static readonly object Instancelock = new object();
+        private dataManager()
+        {
+            //do something
+
+
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        private static dataManager instance = null;
+
+        public static dataManager GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (Instancelock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new dataManager();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
     }
 }
